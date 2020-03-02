f66dd80ff6805d39a67f186ffef2eb50961a2f79
tiagostryke <tiagostryke@gmail.com>
2020-03-02 18:57:13 -0300

    Tutorial pop up finished

--- app/src/main/java/com/example/rpsls/MainActivity.java
+++ app/src/main/java/com/example/rpsls/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.rpsls;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton buttonHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonHelp = (ImageButton) findViewById(R.id.buttonHelp);
+
+        buttonHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), PopActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
+
 }
---
+++ app/src/main/java/com/example/rpsls/PopActivity.java
@@ -0,0 +1,47 @@
+package com.example.rpsls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+public class PopActivity extends Activity {
+
+    ImageButton buttonExitTutorial;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop);
+
+        buttonExitTutorial = (ImageButton) findViewById(R.id.buttonExitTutorial);
+        buttonExitTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.9),(int) (height*.7));
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+
+        getWindow().setAttributes(params);
+
+
+    }
+}
